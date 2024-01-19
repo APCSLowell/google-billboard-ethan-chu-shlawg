3ccb9b7721bb0a504801134c20ce5b7f7dcf3083
ethan-chu-shlawg <143014840+ethan-chu-shlawg@users.noreply.github.com>
2024-01-18 20:41:31 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,9 +1,8 @@
 public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		GoogleBillboard bob = new GoogleBillboard();
+	public final String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public void main(final String[] args) {
 		 for(int i = 10; i < e.length(); i++){
-		   if(bob.isPrime(Double.parseDouble(e.substring(i-10, i)))){
+		   if(isPrime(Double.parseDouble(e.substring(i-10, i)))){
 		     System.out.println(e.substring(i-10, i));
 		     break;
 		   }
